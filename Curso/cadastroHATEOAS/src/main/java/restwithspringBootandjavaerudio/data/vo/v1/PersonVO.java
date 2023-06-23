e804be40265a34bc81124380742634e9e64c1d79
Pedro Henrique Pereira <pedrohp36@gmail.com>
2023-06-23 19:10:48 -0300

    Adicionando e testando HATEOAS

---
+++ Curso/cadastroHATEOAS/src/main/java/restwithspringBootandjavaerudio/data/vo/v1/PersonVO.java
@@ -0,0 +1,87 @@
+package restwithspringBootandjavaerudio.data.vo.v1;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@JsonPropertyOrder({"id", "firstName", "lastName", "address", "gender"})
+public class PersonVO extends RepresentationModel<PersonVO> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("id")
+    @Mapping("id")
+    private long key;
+
+//    @JsonProperty("first_name")
+    private String firstName;
+
+//    @JsonProperty("last_name")
+    private String lastName;
+
+    private String address;
+
+    //@JsonIgnore
+    private String gender;
+
+    public PersonVO() {}
+
+    public long getKey() {
+        return key;
+    }
+
+    public void setKey(long key) {
+        this.key = key;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PersonVO personVO)) return false;
+        if (!super.equals(o)) return false;
+        return key == personVO.key && Objects.equals(firstName, personVO.firstName) && Objects.equals(lastName, personVO.lastName) && Objects.equals(address, personVO.address) && Objects.equals(gender, personVO.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), key, firstName, lastName, address, gender);
+    }
+}
