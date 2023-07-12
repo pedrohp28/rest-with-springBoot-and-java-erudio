8aaed78dff8a01b553417442066789f6370596fc
Pedro Henrique Pereira <pedrohp36@gmail.com>
2023-07-11 21:48:35 -0300

    trocando a porta de testes

--- Curso/cadastroDocker/src/test/java/restwithspringBootandjavaerudio/configs/TestConfigs.java
+++ Curso/cadastroDocker/src/test/java/restwithspringBootandjavaerudio/configs/TestConfigs.java
@@ -2,7 +2,7 @@ package restwithspringBootandjavaerudio.configs;
 
 public class TestConfigs {
 
-	public static final int SERVER_PORT = 8080;
+	public static final int SERVER_PORT = 8888;
 
 	public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
 	public static final String HEADER_PARAM_ORIGIN = "Origin";
