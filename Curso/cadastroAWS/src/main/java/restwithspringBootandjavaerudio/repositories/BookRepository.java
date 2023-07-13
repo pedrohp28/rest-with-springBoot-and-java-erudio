0e4688e410de4d4c42268093c59210891c85e93a
Pedro Henrique Pereira <pedrohp36@gmail.com>
2023-07-13 20:28:45 -0300

    testando o deploy

--- Curso/cadastroAWS/src/main/java/restwithspringBootandjavaerudio/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package restwithspringBootandjavaerudio.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import restwithspringBootandjavaerudio.model.Book;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- Curso/cadastroAWS/src/main/resources/db/migration/V2__Populete_Table_Person.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-    (1,'Itu - São Paulo - Brasil','Pedro Henrique','Male','Pereira'),
-    (2,'Campinas - São Paulo - Brasil','Hugo','Male','Xaves'),
-    (3,'Marte - São Paulo - Brasil','Ana','Famele','Rodrigues'),
-    (4,'Lululandia - US','Jooj','Male','Bagre');
-
